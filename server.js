b01ff5b410e37ae795cffb8dec702fddc2edee7a
Andrew <drewwalsh1@gmail.com>
2016-01-11 19:29:25 +0000

    Final ver

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-var http = require('http');
-var path = require('path');
-
-var async = require('async');
-var socketio = require('socket.io');
 var express = require('express');
+var app = express();
+var routes = require('./app/routes/routes.js');
 
-var router = express();
-var server = http.createServer(router);
+routes(app);
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Server listening at", addr.address + ":" + addr.port);
+var port = process.env.PORT || 8080;
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
 });
